4f998fb2b7b340012d50eda52c54809f88cb2260
pedrokarsan <104169985+pedrokarsan@users.noreply.github.com>
2025-02-25 17:17:00 +0000

    Update XCUIElementExtension.swift

--- MovieSwift/MovieSwiftUITests/Extension/XCUIElementExtension.swift
+++ MovieSwift/MovieSwiftUITests/Extension/XCUIElementExtension.swift
@@ -9,17 +9,22 @@ import Foundation
 import XCTest
 let defaultTimeout: TimeInterval = 10
 internal extension XCUIElement {
+    // Waits for the element to exist and be hittable within a specified timeout.
     @discardableResult
     func waitToExistAndBeHittable(_ timeToWait: TimeInterval = defaultTimeout) -> Bool {
         let exists = NSPredicate(format: "exists == true AND isHittable == true")
         return waitForExpectationsBase(predicate: exists, element: self, timeToWait: timeToWait)
     }
+
+    // Waits for the element to exist within a specified timeout.
+    @discardableResult
     func waitToExist(_ timeToWait: TimeInterval = defaultTimeout) -> Bool {
         let exists = NSPredicate(format: "exists == true")
         return waitForExpectationsBase(predicate: exists, element: self, timeToWait: timeToWait)
     }
 }
 private extension XCUIElement {
+    // Waits for the element to meet expectation within a specified timeout.
     func waitForExpectationsBase(predicate: NSPredicate, element: XCUIElement, timeToWait: TimeInterval) -> Bool {
         let expect = XCTNSPredicateExpectation(predicate: predicate, object: element)
         let result = XCTWaiter.wait(for: [expect], timeout: timeToWait)
